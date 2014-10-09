11b82b0721e7d00715cdfeebcf781a87e92a6511
JoshHavens <thejrh@ymail.com>
2014-10-08 20:08:32 -0500

    80 percent assignment done

--- src/com/example/mapapp/MainActivity.java
+++ src/com/example/mapapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.mapapp;
-import com.google.android.maps.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+
+
+import android.location.*;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-	String ApiKey = "AIzaSyDagRX70fPyES6A8IX7ZUVOOX8eRJ990GU";
+public class MainActivity extends Activity 
+{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +51,27 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onClick(View view) throws IOException
+	{
+		EditText text = (EditText) findViewById(R.id.info);
+		String address = text.getText().toString();
+		Geocoder earth = new Geocoder(MainActivity.this, Locale.ENGLISH);
+		List<Address> addresses = earth.getFromLocationName(address, 1); //ONLY GETS 1 ADDRESS!!!!
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		MarkerOptions markOp = new MarkerOptions();
+		Address target = addresses.get(0);
+		LatLng loc = new LatLng(target.getLatitude(), target.getLongitude());
+		markOp.position(loc);
+		markOp.title(address); //Only displays on marker click
+		Marker mark = map.addMarker(markOp);
+		CameraPosition cam = new CameraPosition.Builder().target(loc).zoom(14.0f).build(); //Zoom in more/less?
+		CameraUpdate camUp = CameraUpdateFactory.newCameraPosition(cam);
+		map.moveCamera(camUp);
+		//Street View
+		//Current location
+		//Random Location Button
+		//Terrain/Satellite
 	}
 }
